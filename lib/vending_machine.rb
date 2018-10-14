a289d7d443bb42ddb26386398e47c94130b4cb23
Gus <gustavolobo90+cleo@gmail.com>
2018-10-14 15:32:49 +0100

    Add initial structure.

---
+++ lib/vending_machine.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class VendingMachine
+  # Your code goes here...
+end
---
+++ spec/vending_machine_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+RSpec.describe VendingMachine do
+  context 'when initialized with products & coins' do
+    xit 'stores it state' do
+    end
+
+    xit 'allows reload' do
+    end
+  end
+
+  context 'once an item is selected' do
+    context 'and the appropriate amount of money is inserted' do
+      xit 'returns the correct product' do
+      end
+    end
+
+    context 'and too much money is provided' do
+      xit 'returns change' do
+      end
+    end
+
+    context 'and insufficient funds have been inserted' do
+      xit 'asks for more money' do
+      end
+    end
+  end
+end
